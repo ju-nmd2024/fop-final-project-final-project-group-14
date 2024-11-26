e2517ea55f6491269b18b120d7d24da6d8b971ac
cure23 <cure23xv@student.ju.se>
2024-11-26 13:27:53 +0100

    Only inside block collision
    the collision works but doesn´t check which side collides

--- collisionDraft.js
+++ collisionDraft.js
@@ -1,145 +1,225 @@
-let numberColor = 0;
-let colors = [];
-let blocks = [];
+class Block { 
+  constructor(x, y) { 
+    this.x = x; 
+    this.y = y; 
+    this.width = 100; 
+    this.height = 50; 
+  } 
 
-class Block {
-    constructor(x, y, randomColor) {
-      this.x = x;
-      this.y = y;
-      this.randomColor = randomColor;
-      this.width = 450;
-      this.height = 40;
-    }
-  
-    display() {
-      push();
-      translate(this.x, this.y);
-  
-      //assign a different color to the blocks
-      if (this.randomColor === 0){
-        fill(255, 255, 255);
-      } else if (this.randomColor === 1){
-        fill(255, 0, 0);
-      } else if (this.randomColor === 2){
-        fill(0, 255, 0);
-      } else if (this.randomColor === 3){
-        fill(0, 0, 255);
-      }
-      
-      rect(0, 0, this.width, this.height, 10);
-      pop();
-    }
-  }
-  
+  display() { 
+    push(); 
+    translate(this.x, this.y); 
+    fill(255, 255, 255); 
+    rect(0, 0, this.width, this.height, 10); 
+    pop(); 
+  } 
 
+} 
 
-  class Ball {
-    constructor(x, y) {
-      this.x = x;
-      this.y = y;
-      this.width = 20;
-      this.stepX = 2;
-      this.stepY = 2;
-    }
-  
-    collided(){
+class Paddle { 
 
-      // if the ball hits the sides it bounces
-      if (this.x <= this.width/2 || this.x >= 600 - this.width/2) {
-        this.stepX = this.stepX * -1;
-      }
-  
-      if (this.y <= this.width/2 || 
-          this.y >= 600 - this.width/2) {
-        this.stepY = this.stepY * -1;
-      }
-    }
+  constructor(x) { 
+    this.x = x; 
+    this.y = 500; 
+    this.width = 100; 
+    this.weight = 15; 
+  } 
 
+  update() { 
+    if (keyIsDown(39)) { 
+      if (this.x <= 600 - this.width / 2) { 
+        this.x = this.x + 5; 
+      } 
 
-    update() {
-      this.x = this.x + this.stepX;
-      this.y = this.y - this.stepY;
-  
-      
-      
-    }
-  
-    display() {
-      push();
-      translate(this.x, this.y);
-      fill(255, 255, 255);
-      ellipse(0, 0, this.width);
-      pop();
+    } else if (keyIsDown(37)) { 
+      if (this.x >= this.width / 2) { 
+        this.x = this.x - 5; 
+      } 
+    } 
 
-    }
-  }
-  
-  class Paddle {
-    constructor(x) {
-      this.x = x;
-      this.y = 500;
-      this.width = 100;
-    }
-  
-    update() {
-  
-      //paddle movement with arrow keys
-      if (keyIsDown(39)) {
-        if (this.x <= 600 - this.width / 2) {
-          this.x = this.x + 5;
-        }
-      } else if (keyIsDown(37)) {
-        if (this.x >= this.width / 2) {
-          this.x = this.x - 5;
-        }
-      }
-    }
-  
-    display() {
-      push();
-      translate(this.x, this.y);
-      // the x and y refer to the center or the paddle
-      translate(-this.width / 2, 0);
-      stroke(255, 255, 255);
-      strokeWeight(15);
-      line(0, 0, this.width, 0);
-      pop();
-    }
-  }
+  } 
+
+  bigger() { 
+    this.width = 150; 
+  } 
+  smaller() { 
+    this.width = 150; 
+
+  } 
+
+ 
+
+  display() { 
+
+    push(); 
+    translate(this.x, this.y); 
+    translate(-this.width / 2, 0); 
+    stroke(255, 255, 255); 
+    strokeWeight(this.weight); 
+    line(0, 0, this.width, 0); 
+    pop(); 
+
+  } 
+
+} 
+
+ 
+
+class Ball { 
+
+  constructor(x, y) { 
+    this.x = x; 
+    this.y = y; 
+    this.r = 7; 
+    this.stepX = 5; 
+    this.stepY = 5; 
+    this.dead = false; 
+
+  } 
+
+ 
+
+  update() { 
+    this.x = this.x + this.stepX; 
+    this.y = this.y - this.stepY; 
+  } 
 
 
+  display() { 
+    push(); 
+    translate(this.x, this.y); 
+    fill(255, 255, 255); 
+    ellipse(0, 0, this.r * 2); 
+    pop(); 
 
-  function setup() {
-    createCanvas(600, 600);
-  }
-  
-  
-  let ball = new Ball(400, 400);
-  let paddle = new Paddle(200);
-  let start = false;
-  let block = new Block (100, 100);
-  
-  function draw() {
-    background(0, 0, 0);
-    
-    
-    //generate block in the Block class?
-    
-    block.display();
-    //block.update();
+  } 
 
-    
-   /*  for (let block of blocks){
-        if(ball.y >= block.y+block.height){
-           if (ball.x >= block.x || ball.x<=block.x+block.width)
-            block.stepY = block.stepY * -1;
-        }
-    } */
-  
-    ball.display();
-    ball.collided();
-    ball.update();
-  
-    paddle.display();
-    paddle.update();
-}
+ 
+
+  collision(paddle, blocks) { 
+
+    for (let block of blocks) { 
+
+      //ball inside block 
+      if ( 
+        this.x > block.x && 
+        this.x < block.x + block.width && 
+        this.y < block.y + block.height && 
+        this.y > block.y 
+        ) { 
+
+          this.stepX = this.stepX * -1; 
+          blocks.splice(blocks.indexOf(block), 1); 
+
+        } 
+        
+
+      } 
+
+
+//paddle collision 
+
+    if (this.y + this.r >= paddle.y) { 
+      if ( 
+        this.x <= paddle.x + paddle.width / 2 && 
+        this.x >= paddle.x - paddle.width / 2 
+      ){ 
+        //this.stepX = this.stepX * -1; 
+        this.stepY = this.stepY * -1; 
+
+  } else { 
+  this.dead = true; 
+  } 
+    } 
+
+ 
+
+//sides collision 
+
+    if (this.x <= this.r || this.x >= 600 - this.r) { 
+      this.stepX = this.stepX * -1; 
+    } 
+
+    if (this.y <= this.r || this.y >= 600 - this.r) { 
+      this.stepY = this.stepY * -1; 
+    } 
+
+  } 
+
+ 
+
+  isDead() { 
+    if (this.dead === true) {  
+        this.x=paddle.x; 
+        this.y=480; 
+        this.stepY=this.stepY * -1;
+        this.dead=false; 
+
+  } 
+ } 
+} 
+
+function setup() { 
+  createCanvas(600, 600); 
+} 
+
+ 
+
+let ball = new Ball(400, 400); 
+let ball2= new Ball (400,400); 
+let paddle = new Paddle(200); 
+let blocks = []; 
+let start = true; 
+
+ 
+
+ 
+
+function gridBlocks() { 
+
+  for (let i = 0; i < 5; i++) { 
+    for (let j = 0; j < 4; j++) { 
+      let block = new Block(50 + 100 * i, 100 + 50 * j); 
+      blocks.push(block); 
+    } 
+  } 
+} 
+
+ 
+
+function draw() { 
+ background(0, 0, 0); 
+
+  if (start === true) { 
+    gridBlocks(); 
+    start = false; 
+  } 
+
+ 
+
+  let count = 0; 
+
+  for (let block of blocks) { 
+    block.display(); 
+    fill(255, 0, 0); 
+    textSize(30); 
+    text(count, block.x, block.y, 50, 30); 
+    count++; 
+
+  } 
+
+  ball.display(); 
+  ball.collision(paddle, blocks); 
+ 
+
+  ball.isDead(); 
+  ball.update(); 
+
+
+  paddle.display(); 
+  paddle.update(); 
+  //paddle.bigger(); 
+
+  //paddle.smaller(); 
+
+} 
