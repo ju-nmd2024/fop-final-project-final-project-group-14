de619c283140a54c5375ec8563306970af2cb8d9
Dwebbell <bebe24sc@student.ju.se>
2024-11-27 14:52:39 +0100

    Background and roof details
    Done with background loop
    Startinf with roof details

--- tent.js
+++ tent.js
@@ -1,19 +1,33 @@
+let rectX = 100;
+let rectY = 250;
+let roofDetailX = 100;
+let roofDetailY = 250;
+// let rectRedX = 150;
+// let rectRedY = 300;
+
+
 function setup(){
     createCanvas(1200, 900);
     background(255, 255, 255);
 }
  
-function tentBackground() {
+function tentBackground(rectX, rectY) {
     noStroke();
     fill(100, 10, 10);
-    rect(100, 300, 50, 600);
-    fill(210, 215, 215);
-    rect(150, 300, 50, 600);
+    rect(rectX, rectY, 100, 700);
+    fill(200, 215, 215);
+    rect(rectX + 50, rectY, 50, 700);
 }
 
-
+function roofDetail() {
+    fill(100, 10, 10);
+    arc(100, 250, 80, 80, HALF_PI + PI);
+}
 
 function draw () {
-    for (let i = 0; i < 5; i++)
-    tentBackground();
+    for (let i = 2; i < 10; i++){
+        tentBackground(rectX * i , rectY);
+    }
+
+    roofDetail();
 }
