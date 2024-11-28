9b61df3bdad2e7557fde077327ba2d975fbb6b3f
Dwebbell <bebe24sc@student.ju.se>
2024-11-28 20:04:21 +0100

    Jacket/moustache
    Left and right moustache added
    Jacket done

--- circusConductor.js
+++ circusConductor.js
@@ -17,6 +17,16 @@ function circusConductor(){
     ellipse(185, 370, 10, 12);
     ellipse(215, 370, 10, 12);
 
+    //jacket
+    push();
+    noStroke();
+    fill(100, 0, 0);
+    triangle(165, 395, 170, 410, 150, 405);
+    triangle(167, 400, 158, 420, 180, 440);
+    triangle(235, 395, 230, 410, 250, 405);
+    triangle(233, 400, 242, 420, 220, 440);
+    pop();
+
     //jacket buttons
     fill(237, 200, 33);
     line(180, 400, 220, 400);
@@ -26,7 +36,20 @@ function circusConductor(){
     ellipse(185, 415, 8);
     ellipse(215, 415, 8);
 
-    //moustache
+    //moustache left
+    beginShape();
+    fill(0, 0, 0);
+    vertex(198, 385);
+    bezierVertex(195, 385, 195, 395, 175, 385);
+    bezierVertex(175, 390, 195, 410, 198, 385);
+    endShape();
+
+    //moustache right
+    beginShape();
+    vertex(202, 385);
+    bezierVertex(205, 385, 205, 395, 225, 385);
+    bezierVertex(225, 390, 205, 410, 202, 385);
+    endShape();
 
     //hat
     push();
